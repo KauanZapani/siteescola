283f505973e65f66adb8fab1403de811ddb44d37
KauanZapani <zapani.kauan@escola.pr.gov.br>
2026-03-18 07:54:56 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
-
-// Mensagem de boas-vindas no console (exemplo de JS)
-console.log("Bem-vindo ao site do Colégio Estadual São Cristóvão!");
